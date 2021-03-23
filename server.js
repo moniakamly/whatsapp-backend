a91314ebff7588e93358af63fd8d30e3226c8095
AYMEN <moniakamli95@gmail.com>
2021-03-23 15:23:34 +0100

    Implement db schema for mongoose

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 // Importing
 import express from 'express';
 import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
+import Pusher from 'pusher';
+import cors from 'cors';
 
 // app config
 const app = express()
 const port = process.env.PORT || 9000
 
+const pusher = new Pusher({
+    appId: "1175648",
+    key: "bec4e8bffacec764f1ed",
+    secret: "2199a4ae71fded516f08",
+    cluster: "eu",
+    useTLS: true
+});
+
 // middleware
+app.use(express.json());
+app.use(cors());
+
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Headers", "*");
+// });
 
 // DB config
 const connection_url = 'mongodb+srv://admin-monia:rootroot123@cluster0.izrrw.mongodb.net/whatsappdb?retryWrites=true&w=majority';
@@ -14,12 +32,60 @@ mongoose.connect(connection_url, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+const db = mongoose.connection
+
+db.once('open', () => {
+    console.log('The DB is connected');
+
+    const msgCollection = db.collection("messagecontents");
+    const changeStream = msgCollection.watch();
+
+    changeStream.on("change", (change) => {
+        console.log('change is', change);
+
+        if (change.operationType === 'insert') {
+            const messageDetails = change.fullDocument;
+            pusher.trigger('messages', 'inserted', 
+                {
+                    name: messageDetails.name,
+                    message: messageDetails.message,
+                    received: messageDetails.received,
+                    timestamp: messageDetails.timestamp,
+                })
+        } else {
+            console.log("Error triggering Pusher");
+        }
+    });
 })
 
 // ??
 
 // api routes
-app.get('/', (req, res) => res.status(200).send('Hello wold'))
+app.get('/', (req, res) => res.status(200).send('Hello Everyooone'));
+
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
 
 // listen
 app.listen(port, () => console.log(`Listening on localhost: ${port}`))
